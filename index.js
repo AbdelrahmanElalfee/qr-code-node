40fe9f284bf596acc5b0043246c90d5189e5ea69
AbdelrahmanElalfee <abdelrahmanelalfee@gmail.com>
2023-11-01 17:31:58 +0200

    post method

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ app.use('/qr-codes', express.static('qr-codes'));
 
 app.get('/', (req, res, next) => {
     res.send('Hello');
-})
+});
 
-app.get('/generate/:url', (req, res) => {
-    const url = req.params.url;
+app.post('/generate', (req, res) => {
+    const url = req.body.url;
     if (!url) {
         return res.status(400).json({ error: 'URL parameter is missing' });
     }
     const currentDate = new Date().toISOString().replace(/[:.-]/g, '_');
-    let filename = `qr-codes/qr-${currentDate}.png`;
+    const filename = `qr-codes/qr-${currentDate}.png`;
 
     qr.toFile(filename, url, {errorCorrectionLevel: 'H', width: 300, version: 10}, (err) => {
-            res.json({'image': filename});
+        res.json({'message': 'Generated successfully.', 'image': filename});
     });
 });
 
